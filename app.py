7fc424f7191f1b128e6f8de158a8fc155a9ea590
is4cx23 <00001109701159sp@al.educacao.sp.gov.br>
2024-09-05 12:01:57 -0300

    pio

--- app.py
+++ app.py
@@ -3,23 +3,23 @@ import requests
 
 app = Flask(__name__)
 
-API_URL ='http://127.0.0.1:5000/api/livros'
+API_URL = 'http://127.0.0.1:5000/api/livros'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method =='POST':
-        Titulo = request.form.get('titulo')
+    if request.method == 'POST':
+        titulo = request.form.get('titulo')
         autor = request.form.get('autor')
 
-        response = requests.post(API_URL, json={'titulo':Titulo, 'autor': autor})
-
+        response = requests.post(API_URL, json={'titulo':titulo, 'autor': autor})
+    
     response = requests.get(API_URL)
     if response.status_code == 200:
-           livros = response.json()
+        livros = response.json()
     else:
-            livros = []
+        livros = []
 
-    return render_template('index.html',livros=livros)
+    return render_template('index.html', livros=livros)
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
